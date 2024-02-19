4570252b82b13edb34e89376bc3ffe2c94d95dd1
originalcodedev <haris120787@gmail.com>
2024-02-19 20:42:12 +0300

    Task012Test solved

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/UiState.kt
@@ -0,0 +1,27 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.widget.Button
+import android.widget.TextView
+import java.io.Serializable
+
+interface UiState : Serializable {
+
+    fun apply(textView: TextView, button: Button)
+
+    abstract class Abstract(private val text: String) : UiState {
+        override fun apply(textView: TextView, button: Button) {
+            textView.text = text
+        }
+    }
+
+    data class Base(private val text: String) : Abstract(text)
+
+    data class Max(private val text: String) : Abstract(text) {
+        override fun apply(textView: TextView, button: Button) {
+            super.apply(textView, button)
+            button.isEnabled = false
+        }
+
+    }
+
+}
